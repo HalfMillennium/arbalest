25dc560f34a2c55032ed8bee31ab2243c1f66ac5
HalfMillennium <glc2266@gmail.com>
2024-05-18 23:21:12 -0400

    Continue developing component hierarchy for ViewCompaignsPage.

--- src/pages/campaigns/view-campaigns/ViewCampaignsPage.tsx
+++ src/pages/campaigns/view-campaigns/ViewCampaignsPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Card, List, ListItem, Typography } from "@mui/material";
 import { DashboardContent } from "../components/DashboardContent";
+import { OpenInNew } from "@mui/icons-material";
 import "./ViewCampaignsPage.css";
 
 export function ViewCampaignsPage() {
@@ -10,10 +11,37 @@ export function ViewCampaignsPage() {
     <div className="items-center">
       <div className="m-4 h-screen flex flex-row">
         <div className="w-1/5 drawer-bg rounded-lg">
+          <div className="flex flex-col p-4">
+            <div className="flex flex-row justify-start">
+              <div>
+                <Typography
+                  fontFamily={"Radio Canada Big"}
+                  fontWeight={500}
+                  fontSize={32}
+                  color={"white"}
+                >
+                  {/** TODO: Create max character count of about this length */}
+                  How2Builders
+                </Typography>
+              </div>
+              <div
+                className="cursor-pointer"
+                onClick={() => {
+                  console.log("Should open property website");
+                }}
+              >
+                <OpenInNew />
+              </div>
+            </div>
+            <span className="text-base">
+              <Typography fontFamily={"Source Sans 3"} color="#212121">
+                PROPERTY
+              </Typography>
+            </span>
+          </div>
           <List>
             <ListItem>
               <DrawerCard
-                id="campaigns"
                 title="Campaigns"
                 body="Create, edit and view (past or present) campaigns for the selected
           property."
@@ -21,37 +49,35 @@ export function ViewCampaignsPage() {
             </ListItem>
             <ListItem>
               <DrawerCard
-                id="analytics"
                 title="Analytics"
                 body="Track your past & present campaigns. Dowload data as a CSV file."
               />
             </ListItem>
             <ListItem>
               <DrawerCard
-                id="companions"
                 title="Companions"
-                body="Track your past & present campaigns. Dowload data as a CSV file."
+                body="Manage your virtual email director, curator and writer."
               />
             </ListItem>
           </List>
         </div>
-        <div className="w-4/5 p-10 ml-10">
+        <div className="w-4/5 py-5 pr-5 pl-2 ml-10">
           <DashboardContent />
         </div>
       </div>
     </div>
   );
-  function DrawerCard(props: { title: string; body: string; id: string }) {
-    const { title, body, id } = props;
+  function DrawerCard(props: { title: string; body: string }) {
+    const { title, body } = props;
     return (
       <Card
-        className="cursor-pointer hover:animate-pulse bg-white bg-opacity-50 rounded-md text-left p-4"
+        className="hover:animate-pulse bg-white bg-opacity-50 rounded-md text-left p-4 cursor-pointer"
         style={
-          currentTab === id
-            ? { pointerEvents: "none", opacity: "0.4", color: "white" }
-            : {}
+          currentTab === title
+            ? { opacity: 0.2, pointerEvents: "none" }
+            : { opacity: 0.7 }
         }
-        onClick={() => setCurrentTab(id)}
+        onClick={() => setCurrentTab(title)}
       >
         <div className="mb-4">
           <Typography fontFamily="Radio Canada Big" fontSize={18}>
